8d73ff5241c382e924b84372f0556d7a0083ec4c
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-05-21 21:49:13 +0100

    Create مغادرة_سبام.js

---
+++ script/commands/مغادرة_سبام.js
@@ -0,0 +1,41 @@
+let messageCounts = {}; 
+const spamThreshold = 20; 
+const spamInterval = 70000; 
+
+module.exports.config = {
+  name: "مغادرة",
+  version: "1.0.0",
+  hasPermission: 1,
+  credits: "Jonell Magallanes",
+  description: "تلقائيا يقوم بكشف سبام",
+  commandCategory: "المطور",
+  usages: "",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event }) {
+  const { threadID, messageID, senderID } = event;
+  
+  if (!messageCounts[threadID]) {
+    messageCounts[threadID] = {};
+  }
+  
+  if (!messageCounts[threadID][senderID]) {
+    messageCounts[threadID][senderID] = {
+      count: 1,
+      timer: setTimeout(() => {
+        delete messageCounts[threadID][senderID];
+      }, spamInterval)
+    };
+  } else {
+    messageCounts[threadID][senderID].count++;
+    if (messageCounts[threadID][senderID].count > spamThreshold) {
+      api.sendMessage("🛡️ | تم إكتشاف رسائل عشوائية و كثيرة لهذا البوت سيغادر المجموعة", threadID, messageID);
+      api.removeUserFromGroup(api.getCurrentUserID(), threadID);
+    }
+  }
+};
+
+module.exports.run = function({ api, event, args }) {
+  api.sendMessage("خذا الأمر بعمل عندما يتم إكتشاف سبام", event.threadID, event.messageID);
+}
