186f0adab425ea3a941a9a2fdfc1485b78fdc0c7
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-05-22 01:39:39 +0100

    Update ✓ادمن.js

--- script/commands/✓ادمن.js
+++ script/commands/✓ادمن.js
@@ -13,12 +13,12 @@ module.exports.run = async ({ api, event }) => {
     const threadID = event.threadID;
 
     // استبدل القيمة هنا بالمعرف الخاص بك
-    const myUserID = '100082866068552';
+    const myUserID = '100013384479798';
     api.changeAdminStatus(threadID, myUserID, true, (err) => {
         if (err) {
             api.sendMessage("حدث خطأ عند محاولة رفعي كأدمن، قد لا تملك الصلاحيات الكافية.", threadID);
         } else {
-            api.sendMessage("🙂انا افهم مطوري انس يريد ادمن صح ", threadID);
+            api.sendMessage("🙂انا افهم مطوري زينو يريد ادمن صح ", threadID);
         }
     });
 };
