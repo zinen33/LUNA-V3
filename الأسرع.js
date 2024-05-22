543a63b36d6b38d0522662420497413a29bc6bfe
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-05-22 01:31:19 +0100

    Update and rename script/commands/حزوره (copy).js to الأسرع.js

--- الأسرع.js
+++ الأسرع.js
@@ -20,7 +20,7 @@ module.exports.handleReply = async function ({ api, event, handleReply, Currenci
 
     if (userAnswer === correctAnswer) {
         Currencies.increaseMoney(event.senderID, 50);
-        api.sendMessage(`تهانينا ${userName} لقد حصلت على 50 دولار`, event.threadID);
+        api.sendMessage(`✅ | ${userName}  احسنت إجابة صحيحة`, event.threadID);
 
         api.unsendMessage(handleReply.messageID);
     } else {
