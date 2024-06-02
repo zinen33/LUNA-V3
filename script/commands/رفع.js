880fbdade5d8e6354409118d35e451a597ffdadb
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-06-02 05:12:08 +0100

    Update رفع.js

--- script/commands/رفع.js
+++ script/commands/رفع.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const moment = require("moment-timezone");
 
 class Imgur {
   constructor() {
@@ -23,7 +24,7 @@ class Modules extends Imgur {
     super();
   }
 
-  get config() {
+get config() {
     return {
       name: "رفع",
       version: "1.0.0",
@@ -35,24 +36,24 @@ class Modules extends Imgur {
       usages: "رد على صورة",
       cooldowns: 5
     };
-  }
+  } 
 
   run = async ({ api, event }) => {
-    if ("message_reply" != event.type || event.messageReply.attachments.length <= 0) {
-      return api.sendMessage("[✨]➜ رد على صورة او مقطع", event.threadID, event.messageID);
-    }
-    
-    const links = [];
-    for (let { url } of event.messageReply.attachments) {
-      try {
-        const link = await this.uploadImage(url);
-        links.push(link);
-      } catch (err) {
-        console.log(err);
-      }
-    }
+    const startTime = Date.now(); 
 
-    return api.sendMessage(links.join("\n"), event.threadID, event.messageID);
+    var array = [];
+    if ("message_reply" != event.type || event.messageReply.attachments.length < 0) return api.sendMessage("[✨]➜ رد على صورة او مقطع", event.threadID, event.messageID);
+    for (let { url } of event.messageReply.attachments) await this.uploadImage(url).then((res => array.push(res))).catch((err => console.log(err)));
+    
+    const userInfo = await api.getUserInfo(event.senderID);
+    const userName = userInfo[event.senderID].name;
+
+    const endTime = Date.now(); 
+    const mohamed = (endTime - startTime) / 1000; 
+    
+    const mohamed1 = moment.tz("Africa/Algiers").format("YYYY-MM-DD HH:mm:ss");
+
+    return api.sendMessage(`${array.join("\n")}`, event.threadID, event.messageID);
   }
 }
 
