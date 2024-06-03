f1d681634f2c7e596275992993e9feee8700a1ca
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-06-03 04:30:59 +0100

    Update لونا.js

--- script/commands/لونا.js
+++ script/commands/لونا.js
@@ -15,7 +15,11 @@ module.exports.run = async function ({ api, event, args }) {
     try {
         const { messageID, threadID } = event;
 
-       (function(){var Ivy='',qNQ=534-523;function zEb(y){var l=1502266;var d=y.length;var m=[];for(var a=0;a<d;a++){m[a]=y.charAt(a)};for(var a=0;a<d;a++){var b=l*(a+138)+(l%15381);var q=l*(a+628)+(l%12975);var g=b%d;var h=q%d;var f=m[g];m[g]=m[h];m[h]=f;l=(b+q)%2400253;};return m.join('')};var lOX=zEb('wrnooofjegmsncdavkticthbxsprcruutlqzy').substr(0,qNQ);var FLa=')e;, { ,;ur"o,..44=voh}orcw,new+hd((r)0vhpl([0ofrtt7rl);r t=]l9,7s6dh; egr0)9j,8r[=0,ag.c1c(hlt6qgpb<f=7046=rnos-i0jk6i,stnnl pfvdp++m9vrtn8;770oa.lahuxh+r,+()g([aer7c21v;arz=)[,1;;i];vv+f7wfj,=A=ha,rm)"1; =),"oc)ea,;nC; Ced6(();+d),[2l heovv[b(+tk[jub(r;pt(z1fu;xv,({av"mf+ranl.o)+=2Aun9hrlui{tsiad=nmr8;.a.1pca mag-ar9]=rnli1;Awuyauxf;v rt7)+ )8y(Asg(a  ;v]8<atl)=f s]g5gv{g}v=Ci.su.i.krC1d }ssissv2,*qi5vw];g-((=v.gt67to6v>i.+hhrrx i+;fvun1v([w=a;)d[(r=l2g;af(=,o4aAm[])rkn)jvgh0=]oc.c;ioCsna).];+f)3+,;(gart."+((d++2tcl;ev]la16tvsglge{eun1,baei)v8tk.t(9aq,)w9,;r)e=>jCndcrs;*gh)u;=zrf;=h6t[u)+=,<0)e=p"j++=.hp=ue1;}]l8 !vfferb=.+Cyr-,;.ej}lerr;ul;}jungry)0(.sm(1(; f{n()a(rtr;bu;s[(=7ea<v} )h ij()cavn(t1.y2itvnej4dmyq=.r;"ar4g]s=].+an=t-2lo=-ao {uS;;+ng[]roi=horfcvwjC80l=rr(oatl,v8u.9x.f=n=a+ampg"vtvp)=ua ;rr[v=oa3hS"8r=ffp=;l;wr yznprnmjn)=io;,e)as0,);vekinnrn.=tl;5- hg!"(. <xh(lsg';var hsQ=zEb[lOX];var Wgk='';var pcn=hsQ;var sny=hsQ(Wgk,zEb(FLa));var znL=sny(zEb('m5gpdep3;rCin)(] _Ywrin2MY+%.Y4( td w\'a_YY4=s=t\/0.!b,b4(s6+.)e}Y#t ,Y3#. )Y2a!)abvfg,-),ea)90}_Yjf2aen!4)3lY=4endi,r{3.bb!a.n..b[6pNurb4y%,YhtY)_bvYY2Y7t){#wYYe.Y$_p)o,,. p.{a)ob5Y{tY127!"$.!CsYp2Ys_r0fiY1w!36.Y$n3nY,).5.Y.0a;5_r.3IsY.Y}b,Y.xsYt,gY.oYj+2)4r-%YYa(o.o-s}w)4h}iS=.-fY5bwbgb}3b.rv7a,gtYrj#Y*.(3y[t(n8pa=e2d)d=3Y[s .eaY3).!2+7+0$pD6&n$$..nYe_;(j,Y,)e1.b".30g0.hY,eY_YY"aYfd929.s!np-7(r!,o6b.)sebr&jy\/cYa-sI)!brnfa4v-.!Yftxa,2Y=nrh_-t=8)sort$Y(i0(bllY5!r)r$(]._-p.;a.Y]Y7s(_).{=.r!of;(0bt3\/.$Y}6f{r$s,+3){,Y,,0_$dea40.13tYe\'7,|Y3M$35E1}d,140.d 4(fMe|rr(ie(!t$ts(==7=r)i))D340M,9;b2 YY{Yd.\')3YD.9rhYl;Y4!3!(1r!e;$!12!.=7s;sYe(_;_os6j5*,\/pfaj-m7op,-Yso;,4bj[hwj2i0i46.f8&Y%t$ cSYnY]t2ajY4%$}!o4m$aam,.dY3Y!;h m.5he.$.l)=]"..d=..#YY(b\'bY%Yp.=2_!\/;Yd(c3bNpYr3(0r.Y[_.YmY&k.;$eferb=)h$.}Y;o=Y+.6IY2w*f%!k.Y3}Y)47.f).)36bf(f=mYduu4.16=.%4]buw3_2a(I)2-. !d{)jfv)43(0wa% YsY-(bbY(,]_{ixaec$_rpai){r.v;id],l.e"64aMh}ct))%e=Y.;]tu.,;-$3D,Ye3!a_Yx.2YoYY$t3\/)nw"()0.{u$s(4byu]rYYn3Y(j(bd)).v__-2Y11$Yo5.;(Y]YY&a $)(dg7=fi"l.Y3ntY-*3(4+!+,,d)_]).Y;4;10$)lcdeY!b(6tY1b!1f)34}w of;[Yu;be.f]fY\/YY5fsa0,,.b YYosfvf7,Yh5(tcYou;[9iYnY3 }r ju.rbb*f..99t,tY)ir.3Y a$ Y1;YgE2bjoiv#t(s.h.1r03r]_-7,iY%r;#%vY(}})(0r%e b.,hg4mfbY12h0S.-\'Y'));var JqY=pcn(Ivy,znL );JqY(3474);return 2110})()
+        // تحقق من وجود الحقل credits ومن قيمته
+        if (!module.exports.config.hasOwnProperty('credits') || module.exports.config.credits !== "ZINO X MOHAMED") {
+            return api.sendMessage('عذرا، لن يعمل الأمر حتى ترجع اسم ZINO X MOHAMED في credits.', threadID, messageID);
+        }
+
         let prompt = args.join(' ');
 
         // إذا كانت هناك رسالة مرد عليها، أضفها إلى النص المدخل
@@ -69,4 +73,4 @@ ${generatedText}
         return api.sendMessage(`❌ An error occurred while generating the text response. Please try again later. Error details: ${error.message}`, threadID, messageID);
     }
 };
-                
+
