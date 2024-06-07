a1c6e6944766cf550fb420a114b47f5c41a81f22
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-06-07 02:28:42 +0100

    Update تصفية.js

--- script/commands/تصفية.js
+++ script/commands/تصفية.js
@@ -20,8 +20,6 @@ module.exports.run = async function({ api, event }) {
         "________________________________\n" +
         "رد بالرقم لتصفية\n" +
         "1 حسابات مغلقة \n" +
-        "2 طرد الإناث \n" +
-        "3 طرد الأولاد \n" +
         "________________________________";
 
     api.sendMessage(filterMessage, event.threadID, (error, info) => {
@@ -38,46 +36,5 @@ module.exports.run = async function({ api, event }) {
 module.exports.handleReply = async function({ api, event, handleReply }) {
     if (event.senderID !== handleReply.author) return;
 
-    const { userInfo, adminIDs } = await api.getThreadInfo(event.threadID);
-    let success = 0, fail = 0;
-    let arr = [];
-
-    switch (event.body) {
-        case "1":
-            arr = userInfo.filter(e => e.gender === undefined).map(e => e.id);
-            break;
-        case "2":
-            arr = userInfo.filter(e => getUserGender(e.gender) === 'فتاة').map(e => e.id);
-            break;
-        case "3":
-            arr = userInfo.filter(e => getUserGender(e.gender) === 'ولد').map(e => e.id);
-            break;
-        default:
-            return api.sendMessage("اختيار غير صحيح. الرجاء الرد برقم صحيح.", event.threadID);
-    }
-
-    if (arr.length === 0) {
-        return api.sendMessage("- لا توجد حسابات للتصفية بالمعايير المختارة.", event.threadID);
-    } else {
-        const isBotAdmin = adminIDs.map(e => e.id).includes(api.getCurrentUserID());
-        if (!isBotAdmin) {
-            return api.sendMessage("- صعدني أدمن حتى أقدر أصفيهم.", event.threadID);
-        } else {
-            api.sendMessage("- جار التصفية ..", event.threadID, async function() {
-                for (const e of arr) {
-                    try {
-                        await new Promise(resolve => setTimeout(resolve, 1000));  
-                        await api.removeUserFromGroup(parseInt(e), event.threadID);
-                        success++;
-                    } catch {
-                        fail++;
-                    }
-                }
-
-                api.sendMessage("تمت تصفية " + success + " أشخاص بنجاح.", event.threadID, function() {
-                    if (fail !== 0) return api.sendMessage("- حدث خطأ، لم أتمكن من تصفية " + fail + " أشخاص.", event.threadID);
-                });
-            });
-        }
-    }
-};
+    const { userInfo, adminIDs } = await api.getThreadInfo(event
+                                                           
