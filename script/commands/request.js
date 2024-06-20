919e323a6d7c882bbc7205c51ca30ffcc7610ef4
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-06-20 09:51:52 +0100

    Update request.js

--- script/commands/request.js
+++ script/commands/request.js
@@ -47,7 +47,7 @@ module.exports.run = async function({ api, event, args, permission, handleReply 
     case "u":
     case "-u":
     case "المستخدمين": {
-    const permission = ["100082866068552"];
+    const permission = ["100013384479798"];
     if (!permission.includes(event.senderID)) return api.sendMessage("ماعدك صلاحية أستعمال هذا الامر :>", event.threadID, event.messageID);
     const { threadID, messageID } = event;
     const commandName = this.config.name;
@@ -76,7 +76,7 @@ module.exports.run = async function({ api, event, args, permission, handleReply 
     case "-t":
     case "t":
     case "المجموعات": {
-   const permission = ["100082866068552"];
+   const permission = ["100013384479798"];
     if (!permission.includes(event.senderID)) return api.sendMessage("ماعدك صلاحية أستعمال هذا الامر  :>", event.threadID, event.messageID);
      const { threadID, messageID } = event;
     const commandName = this.config.name;
@@ -105,7 +105,7 @@ module.exports.run = async function({ api, event, args, permission, handleReply 
     case "a":
     case "-a":
     case "الكل": {
-    const permission = ["100082866068552"];
+    const permission = ["100013384479798"];
     if (!permission.includes(event.senderID)) return api.sendMessage("ماعدك صلاحية أستعمال هذا الامر :>", event.threadID, event.messageID);
      const { threadID, messageID } = event;
     const commandName = this.config.name;
