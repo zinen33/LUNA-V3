4cb02be591f6c8a122513266873ad5a79e74880f
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-06-24 18:54:32 +0100

    Update كنية.js

--- script/commands/كنية.js
+++ script/commands/كنية.js
@@ -1,37 +1,37 @@
 module.exports.config = {
     name: "كنية",
-    version: "1.0.4",
+    version: "1.0.5",
     hasPermssion: 0,
-    creditss: "MOHAMED X ZINO",
+    credits: "MOHAMED X ZINO",
     MOHAMED: true,
-    usePrefix:true,
+    usePrefix: true,
     description: "وييي",
     commandCategory: "النظام",
     usages: "كنية",
     cooldowns: 5
 };
 
-
-module.exports.handleEvent = async function ({ api, args, event, client, __GLOBAL, Threads, Currencies }) {
+module.exports.handleEvent = async function ({ api, event, client, Threads }) {
     const { threadID } = event;
-    let { nicknames } = await api.getThreadInfo(event.threadID)
-    const nameBot = nicknames[api.getCurrentUserID()]
-    if (nameBot !== `[ ${config.PREFIX} ] • ${config.BOTNAME}`) {
-        api.changeNickname(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "Made by CatalizCS and SpermLord" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+    let { nicknames } = await api.getThreadInfo(threadID);
+    const botNickname = nicknames[api.getCurrentUserID()];
+    const correctNickname = `[ ${global.config.PREFIX} ] • ${global.config.BOTNAME || "Made by CatalizCS and SpermLord"}`;
+
+    if (botNickname !== correctNickname) {
+        api.changeNickname(correctNickname, threadID, api.getCurrentUserID());
         setTimeout(() => {
-            return api.sendMessage(`تم تغيير كنية لونا تلقائيا يرجى عدم العبث بها 🙆🏻‍♀️✨`, threadID);
+            api.sendMessage(`تم تغيير كنية البوت تلقائيًا يرجى عدم العبث بها 🙆🏻‍♀️✨`, threadID);
         }, 1500);
     }
-}
+};
 
-module.exports.run = async({ api, event, Threads}) => {
+module.exports.run = async ({ api, event, Threads }) => {
     let data = (await Threads.getData(event.threadID)).data || {};
-    if (typeof data["cnamebot"] == "undefined" || data["cnamebot"] == false) data["cnamebot"] = true;
+    if (typeof data["cnamebot"] === "undefined" || data["cnamebot"] === false) data["cnamebot"] = true;
     else data["cnamebot"] = false;
 
     await Threads.setData(event.threadID, { data });
     global.data.threadData.set(parseInt(event.threadID), data);
 
-    return api.sendMessage(`✅ ${(data["cnamebot"] == true) ? "Turn on" : "Turn off"} successfully cnamebot!`, event.threadID);
-
-}
+    return api.sendMessage(`✅ ${(data["cnamebot"] === true) ? "تم تفعيل" : "تم تعطيل"} تغيير الكنية التلقائي للبوت!`, event.threadID);
+};
