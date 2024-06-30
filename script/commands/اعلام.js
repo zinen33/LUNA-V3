4e2eb078b8f5399cbfe780fa7abdf7d4a6a95f9e
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-06-30 19:38:18 +0100

    Update اعلام.js

--- script/commands/اعلام.js
+++ script/commands/اعلام.js
@@ -67,4 +67,37 @@ async function sendGameMessage(api, threadID) {
         { image: "https://i.pinimg.com/236x/71/16/33/711633aa590dba2b6b55c5dec8cf00a8.jpg", answer: "ماليزيا" },
         { image: "https://i.pinimg.com/236x/2a/cb/7d/2acb7d9371550e4f145d5a1a841a41cb.jpg", answer: "فيتنام" },
         { image: "https://i.pinimg.com/236x/2c/60/86/2c608693f21531817c6b10129840e9b3.jpg", answer: "المك" },
-        
+        { image: "https://i.pinimg.com/236x/2c/60/86/2c608693f21531817c6b10129840e9b3.jpg", answer: "المكسيك" },
+        { image: "https://i.pinimg.com/564x/00/b5/15/00b515e2b899ce0519d77be8b8e05b54.jpg", answer: "أستراليا" },
+        { image: "https://i.pinimg.com/564x/0c/71/98/0c7198e4b4b7abcc5b5cbded6740322b.jpg", answer: "إسبانيا" },
+        { image: "https://i.pinimg.com/564x/08/10/ed/0810ed7e7bcff7c7551e36283b49da1c.jpg", answer: "إيطاليا" },
+        { image: "https://i.pinimg.com/564x/9f/29/5e/9f295e5a5c15f537567a4d84b33f07d8.jpg", answer: "نيوزيلندا" }
+    ];
+
+    const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+    const response = await axios.get(randomQuestion.image, { responseType: 'arraybuffer' });
+    fs.writeFileSync(tempImageFilePath, Buffer.from(response.data, 'utf-8'));
+
+    const gameMessage = {
+        body: "🔍 | احزر اسم الدولة من العلم في الصورة:",
+        attachment: fs.createReadStream(tempImageFilePath)
+    };
+
+    api.sendMessage(gameMessage, threadID, (error, messageInfo) => {
+        if (error) return console.error(error);
+
+        api.sendMessage(`💡 | تم تشغيل لعبة "احزر العلم"!`, threadID); // إشعار عند تشغيل اللعبة لجميع المجموعات
+        global.client.handleReply.push({
+            type: "guessFlag",
+            name: this.config.name,
+            messageID: messageInfo.messageID,
+            correctAnswer: randomQuestion.answer,
+            author: api.getCurrentUserID()
+        });
+    });
+}
+
+module.exports.run = async function ({ api, event }) {
+    await sendGameMessage(api, event.threadID);
+};
+            
