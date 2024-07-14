64086c481ef8fa2bb3be0e1dd0b082f5d8c7c89d
ZINO Luna developer <156739195+zinen33@users.noreply.github.com>
2024-07-14 09:46:52 +0100

    Update handleCommand.js

--- includes/handle/handleCommand.js
+++ includes/handle/handleCommand.js
@@ -1,23 +1,29 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
-   const stringSimilarity = require('string-similarity'),
-        escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
-        logger =  require("../../utils/log.js");
-    const moment = require("moment-timezone");
-    return async function ({ event }) {
+  const stringSimilarity = require('string-similarity'),
+    escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
+    logger = require("../../utils/log.js");
+  const axios = require('axios')
+  const moment = require("moment-timezone");
+  return async function ({ event }) {
     const dateNow = Date.now()
-    const time = moment.tz("Asia/Kolkata").format("HH:MM:ss DD/MM/YYYY");
-    const { allowInbox, PREFIX, ADMINBOT, DeveloperMode, adminOnly } = global.config;
-
+    const time = moment.tz("Africa/Nouakchott").format("HH:MM:ss DD/MM/YYYY");
+    const { allowInbox, PREFIX, ADMINBOT, NDH, DeveloperMode, adminOnly, keyAdminOnly, ndhOnly,adminPaOnly } = global.config;
     const { userBanned, threadBanned, threadInfo, threadData, commandBanned } = global.data;
     const { commands, cooldowns } = global.client;
     var { body, senderID, threadID, messageID } = event;
-
-    var senderID = String(senderID), 
-        threadID = String(threadID);
+    var senderID = String(senderID),
+      threadID = String(threadID);
     const threadSetting = threadData.get(threadID) || {}
-    
-    const prefixRegex = new RegExp(`^(<@!?${senderID}>|${escapeRegex((threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : PREFIX )})\\s*`);
-        if (!prefixRegex.test(body)) return;
-        if (userBanned.has(senderID) || threadBanned.has(threadID) || allowInbox == ![] && senderID == threadID) {
-            if (!ADMINBOT.includes(senderID.toString())) {
-               
+    const prefixRegex = new RegExp(`^(<@!?${senderID}>|${escapeRegex((threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : PREFIX)})\\s*`);
+    if (!prefixRegex.test(body)) return;
+    const adminbot = require('./../../config.json');
+//// admin -pa /////
+    if(!global.data.allThreadID.includes(threadID) && !ADMINBOT.includes(senderID) && adminbot.adminPaOnly == true)
+    return api.sendMessage("ليس لديك صلاحية العمل في هذه المحادثة، تواصل مع المطور لأجل منح الصلاحية", threadID, messageID)
+    ////end 
+    if (!ADMINBOT.includes(senderID) && adminbot.adminOnly == true) {
+      if (!ADMINBOT.includes(senderID) && adminbot.adminOnly == true) return api.sendMessage('[مــــــود ] - فقط المطور من لديه الصلاحية لأستخدام  البوت', threadID, messageID)
+    }
+    if (!NDH.includes(senderID) && !ADMINBOT.includes(senderID) && adminbot.ndhOnly == true) {
+      if (!NDH.includes(senderID) && !ADMINBOT.includes(senderID) && adminbot.ndhOnly == true) return api.sendMessage('[ مـــــود ] - فقط المطور زينو من يمكنه تفعيله', threadID, messageID)
+     
